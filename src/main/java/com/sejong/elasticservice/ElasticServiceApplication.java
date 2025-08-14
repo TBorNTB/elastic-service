aca2cbb20c41ad6f47a9730713f831cc196cfe2c
PlusUltraCode <kkd06144@naver.com>
2025-08-14 10:47:16 +0900

    first commit

---
+++ src/main/java/com/sejong/elasticservice/ElasticServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sejong.elasticservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticServiceApplication.class, args);
+    }
+
+}
