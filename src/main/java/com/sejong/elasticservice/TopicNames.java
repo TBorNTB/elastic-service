4cc01f8ec2158b61d4de58eac18ce6ebd9072a67
PlusUltraCode <kkd06144@naver.com>
2025-08-27 16:32:59 +0900

    #12 refactor: TopicNames 토픽명 상수 정리

--- src/main/java/com/sejong/elasticservice/TopicNames.java
+++ src/main/java/com/sejong/elasticservice/TopicNames.java
@@ -4,4 +4,5 @@ public final class TopicNames {
     private TopicNames() {}
     public static final String PROJECT_EVENTS = "project-events";
     public static final String DOCUMENT_EVENTS = "document-events";
+    public static final String POSTLIKE_EVENTS = "like-events";
 }
