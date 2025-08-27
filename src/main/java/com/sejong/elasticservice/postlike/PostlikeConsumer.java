7645922d460c24ecbfdf55e5998de4e0fb3cdeaa
PlusUltraCode <kkd06144@naver.com>
2025-08-27 16:32:59 +0900

    #12 feat: PostlikeConsumer 이벤트 처리/매핑 로직 수정

--- src/main/java/com/sejong/elasticservice/postlike/PostlikeConsumer.java
+++ src/main/java/com/sejong/elasticservice/postlike/PostlikeConsumer.java
@@ -1,4 +1,34 @@
 package com.sejong.elasticservice.postlike;
 
+import com.sejong.elasticservice.TopicNames;
+import com.sejong.elasticservice.project.ProjectElasticRepository;
+import com.sejong.elasticservice.project.ProjectIndexEvent;
+import com.sejong.elasticservice.project.Type;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class PostlikeConsumer {
+    private final ProjectElasticRepository projectElasticRepository;
+
+    @KafkaListener(
+            topics = TopicNames.POSTLIKE_EVENTS,
+            groupId = "like-group"
+    )
+    public void consume(String message) {
+
+        PostLikeEvent event = PostLikeEvent.fromJson(message);
+
+        if(event.getPostType()==PostType.PROJECT){
+            projectElasticRepository.update(event.getPostId(), event.getLikeCount());
+        }
+        else if (event.getPostType()==PostType.ARTICLE){
+
+        }
+        else if (event.getPostType()==PostType.NEWS){
+
+        }
+    }
 }
