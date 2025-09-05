d4d0e10a36c47726d5baf88b966f37627f2c7561
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2025-09-05 17:25:39 +0900

    feat: 좋아요, 조회수 consumer 구현

--- src/main/java/com/sejong/elasticservice/postlike/consumer/PostlikeConsumer.java
+++ src/main/java/com/sejong/elasticservice/postlike/consumer/PostlikeConsumer.java
@@ -5,6 +5,9 @@ import com.sejong.elasticservice.common.constants.TopicNames;
 import com.sejong.elasticservice.postlike.domain.PostLikeEvent;
 import com.sejong.elasticservice.postlike.PostType;
 import com.sejong.elasticservice.project.repository.ProjectElasticRepository;
+import com.sejong.elasticservice.csknowledge.repository.CsKnowledgeRepository;
+import com.sejong.elasticservice.news.repository.NewsRepository;
+import com.sejong.elasticservice.document.repository.DocumentElasticRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -13,6 +16,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class PostlikeConsumer {
     private final ProjectElasticRepository projectElasticRepository;
+    private final NewsRepository newsRepository;
+    private final CsKnowledgeRepository csKnowledgeRepository;
+    private final DocumentElasticRepository documentElasticRepository;
 
     @KafkaListener(
             topics = TopicNames.POSTLIKE,
@@ -25,11 +31,14 @@ public class PostlikeConsumer {
         if(event.getPostType()== PostType.PROJECT){
             projectElasticRepository.updateLikeCount(event.getPostId(), event.getLikeCount());
         }
-        else if (event.getPostType()==PostType.ARTICLE){
+        else if (event.getPostType()==PostType.DOCUMENT){
 
         }
         else if (event.getPostType()==PostType.NEWS){
-
+            newsRepository.updateLikeCount(event.getPostId(), event.getLikeCount());
+        }
+        else if (event.getPostType()==PostType.CSKNOWLEDGE){
+            csKnowledgeRepository.updateLikeCount(event.getPostId(), event.getLikeCount());
         }
     }
 }
--- src/main/java/com/sejong/elasticservice/view/consumer/ViewConsumer.java
+++ src/main/java/com/sejong/elasticservice/view/consumer/ViewConsumer.java
@@ -4,6 +4,9 @@ import com.sejong.elasticservice.common.constants.GroupNames;
 import com.sejong.elasticservice.common.constants.TopicNames;
 import com.sejong.elasticservice.postlike.PostType;
 import com.sejong.elasticservice.project.repository.ProjectElasticRepository;
+import com.sejong.elasticservice.csknowledge.repository.CsKnowledgeRepository;
+import com.sejong.elasticservice.news.repository.NewsRepository;
+import com.sejong.elasticservice.document.repository.DocumentElasticRepository;
 import com.sejong.elasticservice.view.domain.ViewEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -13,6 +16,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ViewConsumer {
     private final ProjectElasticRepository projectElasticRepository;
+    private final NewsRepository newsRepository;
+    private final CsKnowledgeRepository csKnowledgeRepository;
+    private final DocumentElasticRepository documentElasticRepository;
 
     @KafkaListener(
             topics = TopicNames.VIEW,
@@ -25,11 +31,14 @@ public class ViewConsumer {
         if(event.getPostType()== PostType.PROJECT){
             projectElasticRepository.updateViewCount(event.getPostId(), event.getViewCount());
         }
-        else if (event.getPostType()==PostType.ARTICLE){
+        else if (event.getPostType()==PostType.DOCUMENT){
 
         }
         else if (event.getPostType()==PostType.NEWS){
-
+            newsRepository.updateViewCount(event.getPostId(), event.getViewCount());
+        }
+        else if (event.getPostType()==PostType.CSKNOWLEDGE){
+            csKnowledgeRepository.updateViewCount(event.getPostId(), event.getViewCount());
         }
     }
 }
