dfdd2a47b925b8713a18ab4aa5d8d0dfd2d5146d
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2025-10-14 22:51:29 +0900

    feat: 구독한 카테고리 콘텐츠 조회 api 추가

--- src/main/java/com/sejong/elasticservice/internal/controller/InternalController.java
+++ src/main/java/com/sejong/elasticservice/internal/controller/InternalController.java
@@ -1,8 +1,12 @@
 package com.sejong.elasticservice.internal.controller;
 
+import com.sejong.elasticservice.common.constants.TechCategory;
+import com.sejong.elasticservice.internal.service.InterestContentService;
 import com.sejong.elasticservice.internal.service.PopularContentService;
-import com.sejong.elasticservice.internal.dto.PopularContentResponse;
+import com.sejong.elasticservice.internal.dto.ContentResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import java.util.List;
+import java.util.Locale.Category;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,14 +19,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class InternalController {
 
     private final PopularContentService popularContentService;
+    private final InterestContentService interestContentService;
 
     @GetMapping("/weekly-popular")
     @Operation(summary = "일주일 내 인기 컨텐츠 조회 - 가장 인기있는 콘텐츠 1개 (좋아요*2 + 조회수)")
-    public ResponseEntity<PopularContentResponse> getWeeklyPopularContent() {
-        PopularContentResponse popularContent = popularContentService.getMostPopularContent();
-        if (popularContent == null) {
+    public ResponseEntity<ContentResponse> getWeeklyPopularContent() {
+        ContentResponse content = popularContentService.getMostPopularContent();
+        if (content == null) {
             return ResponseEntity.noContent().build();
         }
-        return ResponseEntity.ok(popularContent);
+        return ResponseEntity.ok(content);
+    }
+
+    @GetMapping("/follow-interest")
+    @Operation(summary = "뉴스레터 구독자의 관심사 컨텐츠 조회")
+    public ResponseEntity<List<ContentResponse>> getInterstingContent(List<TechCategory> categories) {
+        List<ContentResponse> contents = interestContentService.getRandomContentsOf(categories);
+        return ResponseEntity.ok(contents);
     }
 }
