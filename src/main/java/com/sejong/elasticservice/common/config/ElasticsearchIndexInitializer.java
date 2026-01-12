aed8b549795e307b5ac699178d972789e6775d3e
PlusUltraCode <kkd06144@naver.com>
2026-01-12 16:15:36 +0900

    feat(index) : 서버 실행시 index가 없으면 생성하는 초기화 설정

---
+++ src/main/java/com/sejong/elasticservice/common/config/ElasticsearchIndexInitializer.java
@@ -0,0 +1,52 @@
+package com.sejong.elasticservice.common.config;
+
+import com.sejong.elasticservice.domain.csknowledge.domain.CsKnowledgeDocument;
+import com.sejong.elasticservice.domain.document.domain.DocumentDocument;
+import com.sejong.elasticservice.domain.news.domain.NewsDocument;
+import com.sejong.elasticservice.domain.project.domain.ProjectDocument;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.IndexOperations;
+
+@Configuration
+public class ElasticsearchIndexInitializer {
+
+    @Bean
+    public ApplicationRunner createCsKnowledgeIndex(ElasticsearchOperations ops) {
+        return args -> {
+            IndexOperations indexOps = ops.indexOps(CsKnowledgeDocument.class);
+            if(!indexOps.exists()) {
+                indexOps.createWithMapping();
+            }
+        };
+    }
+    @Bean
+    public ApplicationRunner createProjectIndex(ElasticsearchOperations ops) {
+        return args -> {
+            IndexOperations indexOps = ops.indexOps(ProjectDocument.class);
+            if(!indexOps.exists()) {
+                indexOps.createWithMapping();
+            }
+        };
+    }
+    @Bean
+    public ApplicationRunner createNewsIndex(ElasticsearchOperations ops) {
+        return args -> {
+            IndexOperations indexOps = ops.indexOps(NewsDocument.class);
+            if(!indexOps.exists()) {
+                indexOps.createWithMapping();
+            }
+        };
+    }
+    @Bean
+    public ApplicationRunner createDocumentIndex(ElasticsearchOperations ops) {
+        return args -> {
+            IndexOperations indexOps = ops.indexOps(DocumentDocument.class);
+            if(!indexOps.exists()) {
+                indexOps.createWithMapping();
+            }
+        };
+    }
+}
