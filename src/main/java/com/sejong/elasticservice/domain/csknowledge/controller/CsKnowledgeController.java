e4982d4f87063d9493cf3cba600209dc936a5498
PlusUltraCode <kkd06144@naver.com>
2026-01-14 15:31:59 +0900

    feat : username기반 post 조회 api 구현

--- src/main/java/com/sejong/elasticservice/domain/csknowledge/controller/CsKnowledgeController.java
+++ src/main/java/com/sejong/elasticservice/domain/csknowledge/controller/CsKnowledgeController.java
@@ -7,10 +7,7 @@ import com.sejong.elasticservice.domain.project.domain.PostSortType;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
