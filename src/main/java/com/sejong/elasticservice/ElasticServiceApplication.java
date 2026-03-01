9bc85164b2ac3ac86c5cac468d377bb92f534f26
PlusUltraCode <kkd06144@naver.com>
2026-03-01 16:53:45 +0900

    세계시 -> 한국시로 변경

--- src/main/java/com/sejong/elasticservice/ElasticServiceApplication.java
+++ src/main/java/com/sejong/elasticservice/ElasticServiceApplication.java
@@ -1,11 +1,20 @@
 package com.sejong.elasticservice;
 
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ElasticServiceApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ElasticServiceApplication.class, args);
     }
